845c19375e724fd4b38e3e4c7bd4c910309fbdbd
1Anastasia <arnika@1cv8.org.ua>
2017-01-19 18:38:20 +0200

    +

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+		public AudioClip cowSound;
+
+		// Use this for initialization
+		void Start ()
+		{
+				//renderer.enabled = false; /* Makes object invisible */
+				GetComponent<Renderer> ().enabled = false;
+		}
+    
+		// Update is called once per frame
+		void Update ()
+		{
+				/* Get main Input */
+				//if (Input.GetButton ("Fire1"))
+				if (Input.GetKey ("1")) {
+						GetComponent<Renderer> ().enabled = true; /* Makes object visible */
+						/* Play the ray sound */
+						GetComponent<AudioSource> ().Play ();
+				}
+        
+				if (GetComponent<Renderer> ().enabled == true) {
+						transform.position += Vector3.down * (Time.deltaTime * 2);
+				}
+
+				/* Check for out of bounds */
+
+				if (this.transform.position.y < -1.5) {
+						transform.position = new Vector2 (0.08658695f, 0.1924166f); /* Return bullet to original position */
+						GetComponent<Renderer> ().enabled = false;
+				}
+		}
+
+		void OnTriggerEnter2D (Collider2D other)
+		{
+				if (other.gameObject.name == "cow(Clone)") {
+						AudioSource.PlayClipAtPoint (cowSound, transform.position);
+						/* Destroy the cow */
+						Destroy (other.gameObject);
+						transform.position = new Vector2 (0.08658695f, 0.1924166f); /* Return bullet to original position */
+						GetComponent<Renderer> ().enabled = false;
+				}
+		}
+}
